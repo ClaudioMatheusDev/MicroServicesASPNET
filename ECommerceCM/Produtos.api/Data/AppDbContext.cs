76c4775507db595d56c0847fa771f60592737cff
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-03-27 23:10:16 -0300

    :tada: Iniciando Desenvolvimento de uma Ecommerce utilizando Micro services

---
+++ ECommerceCM/Produtos.api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Produtos.api.Models;
+
+namespace Produtos.api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options) { } // Construtor que recebe um objeto do tipo DbContextOptions<AppDbContext>
+                                                                                       //  e passa para a classe base (DbContext) através do construtor base(options)
+
+        public DbSet<Produto> Produtos { get; set; } // Propriedade do tipo DbSet<Produto> que representa a tabela Produtos no banco de dados
+    }
+}
---
+++ ECommerceCM/Produtos.api/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Produtos.api.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty; // Nome do produto //string.Empty é o mesmo que "" ou null
+        public string Categoria { get; set; } = string.Empty; // Categoria do produto //string.Empty é o mesmo que "" ou null
+        public decimal Preco   { get; set; }
+
+    }
+}
